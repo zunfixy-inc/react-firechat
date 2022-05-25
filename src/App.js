1964aee0ee5889006090acb15756fb9acfae6a04
zuzu <47292373+Zunfixy@users.noreply.github.com>
2022-05-25 15:44:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function App() {
         className="flex-shrink-0 flex items-center justify-between px-4 sm:px-8 shadow-md"
         style={{ height: 'var(--topbar-height)' }}
       >
-        <a href="https://alterclass.io/courses/react">
+        <a href="https://google.com/">
           <img src={brandLogo} alt="Family chat" width={150} />
         </a>
         <div className="flex items-center">
